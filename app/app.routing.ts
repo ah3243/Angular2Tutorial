419f750887beedfb686e9357d61969fc743372d2
albert <1c2fffe2@opayq.com>
2016-08-25 18:01:42 +0200

    router-outlet tag problem fixed.

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,14 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 
-
 import { HeroesComponent }      from './heroes.component';
 
 const appRoutes: Routes = [
-    {
-        path: 'heroes',
-        component: 'HeroesComponent'
-    }
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
 ];
+
 
 export const routing = RouterModule.forRoot(appRoutes);
 
